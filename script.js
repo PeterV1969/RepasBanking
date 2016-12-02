3035537cf36a6e34f867909b3ca26b688acc70cf
matusber <bernatmatus@gmail.com>
2016-12-02 16:27:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+//date and time
+ 
+function updateClock(){
+  var myDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var myMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  
+  var newDate = new Date();
+  var hours = newDate.getHours();
+  var minutes = newDate.getMinutes();
+  var seconds = newDate.getSeconds();
+  
+  var day = myDay[newDate.getDay()];
+  var month = myMonth[newDate.getMonth()];
+  var year = newDate.getFullYear();
+  var numberDay = newDate.getDate();
+  
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+  
+  var time = hours + ":" + minutes + ":" + seconds;
+  var fullDate = day + " " + numberDay + " " + month + " " + year;
+  
+  $("#Date").html(fullDate);
+  $("#time").html(time);
+  }
+  
+$(document).ready(function(){     
+  setInterval('updateClock()', 1000);
+});
