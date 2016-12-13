26673834e80ed3ed90dab4ffb51ab4615bed627a
PeterI1984 <ivancenco84@gmail.com>
2016-12-13 19:34:04 +0100

    modified code

---
+++ jquery.table.js
@@ -0,0 +1,214 @@
+(function($){
+
+
+//***INCOME***
+
+
+jQuery.fn.mytable = function ()
+{
+var inCounter=0;
+var exCounter=0;
+
+var arrayInVal = new Array();
+var arrayExVal = new Array();
+
+
+ function getDate() {
+    var d = new Date();
+      dFormat = [d.getDate(),
+                 d.getMonth()+1,
+                 d.getFullYear()].join('/')+' '+
+                [d.getHours(),
+                 d.getMinutes(),
+                 d.getSeconds()].join(':');
+      
+      return dFormat;
+    }
+    
+
+  //Add income
+    $("#btnAddIn").click(function(){
+    
+
+    
+    
+    var income = {
+    	Amount:   $("#amountIn").val(),
+    	Item: 	  $("#itemIn").val(), 
+    	Id: 	  arrayInVal.length+1,
+    };
+
+    arrayInVal.push(income.Amount);
+   
+if(income.Amount != "" && income.Amount !=0) {
+            
+            
+                      if(income.Amount<0)
+                      {
+                      alert("You can't enter negative Value!");
+                      }else{
+
+
+$(".tbody").append("<tr class='income'><td>"+income.Id+"</td>"+"<td>"+income.Amount+"</td>"+"<td>"+income.Item+"</td>"
+                                       +"<td>"+getDate()+"</td>"+"<td><button id='but1' class='glyphicon glyphicon-remove-circle' style='background: red'>Remove</button></td></tr>");
+
+ inCounter += parseFloat(income.Amount);
+
+                           
+                            }
+                            }
+                           });
+
+
+//Remove all data
+   $("#btnRemAllIn").click(function(){
+var r = confirm("Delete all?");
+    if (r == true) {
+    alert("You pressed OK! All income data will be deleted.");
+           
+           $("#dynTabIn tr:not(.dyntab,.expense)").remove();
+                                
+          inCounter = 0;
+          exCounter = 0;
+          var getAb = inCounter - exCounter;
+       $("#actualBalance").text(getAb);
+          } else {
+        alert("You pressed Cancel!");
+        return false;
+        }
+
+    });
+
+
+//Remove current row income
+       $("#dynTabIn").on("click","#but1",function(){
+       	  var r = confirm("Delete row!");
+     if (r == true) {   
+      var curElVal= $(this).closest("tr").find("td:eq(1)").text();
+      
+      $(this).closest("tr").remove();
+            
+      inCounter-=parseFloat(curElVal); 
+        var getAb = inCounter - exCounter;
+       $("#actualBalance").text(getAb); 
+
+         
+         } else {
+        return false;
+        }
+         });  
+                 
+
+
+
+
+
+//***SUM***
+$("#btnAddIn, #btnAddEx").click(function(){
+
+  var getAb = inCounter - exCounter;
+       $("#actualBalance").text(getAb);
+});
+
+
+  
+  
+
+
+      //Add expense
+$("#btnAddEx").click(function(){
+
+var expense = {
+    	Amount: $("#amountEx").val(),
+    	Item:   $("#itemEx").val(), 
+    	Id: 	arrayExVal.length+1,
+    };
+
+     arrayExVal.push(expense.Amount);
+
+     if(expense.Amount != "" && expense.Amount !=0) {
+          
+                          if(expense.Amount<0)
+                          {
+                          alert("You can't enter negative Value!")
+                          }else{
+                                  
+$(".tbody").append("<tr class='expense'><td>"+expense.Id+"</td>"+"<td>"+"-"+expense.Amount+"</td>"+"<td>"+expense.Item+"</td>"
+                                       +"<td>"+getDate()+"</td>"+"<td><button id='but2' class='glyphicon glyphicon-remove-circle' style='background: red'>Remove</button></td></tr>");         
+            
+      
+           
+            exCounter += parseFloat(expense.Amount);
+                       
+              }                                   
+              }
+    });
+
+    //Remove all data
+$("#btnRemAllEx").click(function(){
+
+var r = confirm("Delete all?");
+    if (r == true) {
+    alert("You pressed OK! All expense data will be deleted.");
+             
+           $("#dynTabIn tr:not(.dyntab,.income)").remove();
+          inCounter = 0;                      
+          exCounter = 0;
+          var getAb = inCounter - exCounter;
+       $("#actualBalance").text(getAb);
+           } else {
+        alert("You pressed Cancel!");
+        return false;
+        }
+
+});
+
+
+//Remove current row expense
+            
+              $("#dynTabIn").on("click","#but2",function(){
+            	var r = confirm("Delete row!");
+    if (r == true) {
+          var curElVal=$(this).closest("tr").find("td:eq(1)").text();
+               
+              $(this).closest("tr").remove();
+              
+              exCounter+=parseFloat(curElVal);
+               var getAb = inCounter - exCounter;
+       $("#actualBalance").text(getAb);
+              
+           } else {
+        return false;
+       }    
+         });
+
+ //Show income
+
+$("#showIn").click(function(){
+
+	$(".income *").show();
+      $(".expense *").hide();
+             
+      });  
+
+
+
+ //Show expense
+$("#showEx").click(function(){
+
+
+$(".expense *").show();
+       $(".income *").hide(); 
+      
+      });
+      
+      //***TURNOVER***
+
+$("#showTo").click(function(){
+
+$(".income *,.expense *").show(); 
+        
+});
+
+}
+})(jQuery);
--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
 
   setInterval('updateClock()', 1000);
-
+ /*
 //function that returns date and time in classic format
   function getDate() {
     var d = new Date;
@@ -60,7 +60,9 @@ $(document).ready(function(){
 
     return date + "." + month + "." + year + " " + hours + ":" + minutes + ":" + seconds; 
   }
-
+  */
+  
+  
   //toggling income and expense panels
   $("#btnEnterIn").click(function() {
     $("#insertIn").fadeToggle("300");
@@ -70,100 +72,10 @@ $(document).ready(function(){
     $("#insertEx").fadeToggle("300");
   }); 
 
-  //constructor function 
-  function PersonalAccount() {
-    this._arrayInAmount = [];
-    this._arrayInDesc = [];
-    this._arrayDate = [];
-    this.counter = 0;
-
-    //methods for ***adding*** income amounts, descriptions and dates
-
-    this.addIncomeAmount = function(incomeAmount) {
-        this._arrayInAmount.push(parseFloat(incomeAmount.val()));
-        this.counter += (parseFloat(incomeAmount.val()) || 0);
-    }
-
-    this.addIncomeDesc = function(incomeDesc) {   
-        this._arrayInDesc.push(incomeDesc.val());
-    }
-
-    this.addDate = function() {   
-        this._arrayDate.push(getDate());
-    }
-
-
-    //methods for ***showing*** income amounts and their descriptions
-    this.showIncomeArrayAll = function() {
-      return this._arrayInAmount.join(", ");
-    }
-
-    this.showIncomeArrayFirst = function() {
-      return this._arrayInAmount[0].toString();
-    }
-
-    this.showIncomeArrayLast = function() {
-      return this._arrayInAmount[(this._arrayInAmount.length)-1].toString();
-    }
-
-
-    this.showIncomeDescArrayAll = function() {
-      return this._arrayInDesc.join(", ");
-    }
-
-    this.showIncomeDescArrayFirst = function() {
-      return this._arrayInDesc[0].toString();
-    }
-
-    this.showIncomeDescArrayLast = function() {
-      return this._arrayInDesc[(this._arrayInDesc.length)-1].toString();
-    }
-
-
-    this.showDateAll = function() {
-      return this._arrayDate.join(", ");
-    }
-
-    this.showDateFirst = function() {
-      return this._arrayDate[0].toString();
-    }
-
-    this.showDateLast = function() {
-      return this._arrayDate[(this._arrayDate.length)-1].toString();
-    }
-
-    //method that returns actual balance 
-    this.showActualBalance = function() {
-      return this.counter;
-    }
-  }
-
-  var $amountIn = $('#amountIn');
-  var $descIn = $('#itemIn');
-  var $btnAddIn = $('#btnAddIn');
-  var $btnShowIn = $('#showIn');
-  var $testArea = $('#testArea');
-  var pa = new PersonalAccount();
-
-  $btnAddIn.on('click', function(event) {
-    if ((!$.isNumeric($amountIn.val())) || ($amountIn.val()) == 0) {
-      alert("Input in income text field is invalid. Please enter a number!");   
-      $amountIn.val(""); 
-      $descIn.val("");   
-    } 
-
-    if ($amountIn.val() != "" || $amountIn.val() != 0) {
-      pa.addIncomeAmount($amountIn); 
-      pa.addIncomeDesc($descIn);
-      pa.addDate();
-      $descIn.val("");
-
-   //   Counter += (parseFloat($amountIn.val()) || 0);
-
-    } 
-    $amountIn.val("");
-  });
-
+ $("#dynTabIn").mytable();
+ 
+  
+   
      //highlighting active text field
     $('input[type="text"]').each(function(){
       $(this).focus(function(){
@@ -174,30 +86,6 @@ $(document).ready(function(){
       });
     });
 
-//test of printing values
-//$btnShowIn.on('click', function() {
-//alert("ok");
-// $testArea.html("Array of Incomes: " + pa.showIncomeArrayAll());
-//$testArea.html("Array of Descriptions: " + pa.showIncomeDescArrayAll());
-// $testArea.html("Array of Dates: " + pa.showDateAll());
-//$testArea.html("Actual Balance: " + pa.showActualBalance());
- // });
-
-
-  //button for action of Income,Expence,Turnover
-
-     $("#showIn").click(function(){         
-     $("#dynTabIn").fadeToggle();
-      });
- 
-     $("#showEx").click(function(){
-     $("#dynTabEx").fadeToggle();
-   
-     });
-  
-      $("#showTo").click(function(){
-      $("#dynTabTo").fadeToggle();
-                           });
 
 
 });       //end function ready
